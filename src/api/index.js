e1c708a027c1455d35a8af975c1de43c2ef04689
Faissal <fhattou@simplon.co>
2019-03-01 16:10:45 +0100

    add square images

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,8 @@
 export default {
   GET_ALL_CHAMPIONS:
     'http://ddragon.leagueoflegends.com/cdn/6.24.1/data/fr_FR/champion.json',
-  CHAMPION_IMAGE: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash' // /{champion_0}.jpg
+  CHAMPION_IMAGE_SPLASH:
+    'http://ddragon.leagueoflegends.com/cdn/img/champion/splash', // /{champion_0}.jpg
+  CHAMPION_IMAGE_SQUARE:
+    'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion' // {champion}.png
 };
