c912140b8e36f016229bba91892de9263eb8eb20
Faissal <fhattou@simplon.co>
2019-03-01 17:11:29 +0100

    getChampionDetail() method working

--- src/screens/ChampionDetail/index.js
+++ src/screens/ChampionDetail/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import api from '../../api';
 
-export default function ChampionDetail(props) {
-  const { championName } = props.match.params;
-  console.log(championName);
-  return (
-    <div>
-      <h1>Champion Détail page</h1>
-    </div>
-  );
+class ChampionDetail extends Component {
+  state = {
+    championDetail: null
+  };
+
+  async componentDidMount() {
+    await this.getChampionDetail();
+  }
+
+  getChampionDetail = async () => {
+    const { championName } = this.props.match.params;
+    const { GET_CHAMPION_DETAIL } = api;
+    const { data } = await axios.get(GET_CHAMPION_DETAIL(championName));
+    const championDetail = data.data[championName];
+    this.setState({ championDetail });
+  };
+
+  render() {
+    const { championDetail } = this.state;
+    console.log(championDetail);
+    return (
+      <div>
+        <h1>Champion Détail page</h1>
+        <img src='' />
+      </div>
+    );
+  }
 }
+
+export default ChampionDetail;
