60efa11b549f0a1b86bfbca5fcb6c0e1641decca
Faissal <fhattou@simplon.co>
2019-03-01 17:06:15 +0100

    Refactored api folders object keys to functions

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,13 @@
 export default {
-  GET_ALL_CHAMPIONS:
+  GET_ALL_CHAMPIONS: () =>
     'http://ddragon.leagueoflegends.com/cdn/6.24.1/data/fr_FR/champion.json',
-  CHAMPION_IMAGE_SPLASH:
-    'http://ddragon.leagueoflegends.com/cdn/img/champion/splash', // /{champion_0}.jpg
-  CHAMPION_IMAGE_SQUARE:
-    'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion' // {champion}.png
+  GET_CHAMPION_DETAIL: championName => {
+    return `http://ddragon.leagueoflegends.com/cdn/6.24.1/data/fr_FR/champion/${championName}.json`;
+  },
+  CHAMPION_IMAGE_SPLASH: championName => {
+    return `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championName}_0.jpg`;
+  },
+  CHAMPION_IMAGE_SQUARE: championName => {
+    return `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${championName}.png`;
+  }
 };
