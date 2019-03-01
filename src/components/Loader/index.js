42e6beb6fb03cb174a1cdd827c62845c1cbf52fb
Faissal <fhattou@simplon.co>
2019-03-01 18:15:59 +0100

    Add ciruclar loader for loading screens states

---
+++ src/components/Loader/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Loader() {
+  return (
+    <div className='Loader'>
+      <div className='circle'>
+        <div className='inner' />
+      </div>
+      <div className='circle'>
+        <div className='inner' />
+      </div>
+      <div className='circle'>
+        <div className='inner' />
+      </div>
+      <div className='circle'>
+        <div className='inner' />
+      </div>
+      <div className='circle'>
+        <div className='inner' />
+      </div>
+    </div>
+  );
+}
