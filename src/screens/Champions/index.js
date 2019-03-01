b95b405b34e059398d48db571681f6ecae2ae3b9
Faissal <fhattou@simplon.co>
2019-03-01 19:26:35 +0100

    Fixed responsive and add champions search input

--- src/screens/Champions/index.js
+++ src/screens/Champions/index.js
@@ -7,8 +7,10 @@ import Loader from '../../components/Loader';
 
 class Champions extends Component {
   state = {
+    isLoading: false,
     allChampions: [],
-    isLoading: false
+    displayedChampions: [],
+    searchInputValue: ''
   };
 
   async componentDidMount() {
@@ -22,24 +24,51 @@ class Champions extends Component {
     const { GET_ALL_CHAMPIONS } = api;
     const { data } = await axios.get(GET_ALL_CHAMPIONS());
     const allChampions = Object.values(data.data).map(item => item);
-    await this.setState({ allChampions });
+    await this.setState({ allChampions, displayedChampions: allChampions });
     await this.toggleLoading(false);
   };
 
+  handleSearchInputChange = e => {
+    const searchInputValue = e.target.value;
+    const displayedChampions = this.state.allChampions.filter(
+      champ =>
+        champ.name.toLowerCase().indexOf(searchInputValue.toLowerCase()) !== -1
+    );
+    this.setState({
+      displayedChampions,
+      searchInputValue
+    });
+  };
+
   render() {
-    const { allChampions, isLoading } = this.state;
+    const { allChampions, displayedChampions, isLoading } = this.state;
 
     if (isLoading) return <Loader />;
 
     return (
       <div className='Champions'>
-        <ul>
-          {allChampions.map(champ => (
-            <li key={champ.key}>
-              <ChampCard {...champ} />
-            </li>
-          ))}
-        </ul>
+        <div className='Champions__header'>
+          <h1>Champions</h1>
+          <input
+            onChange={this.handleSearchInputChange}
+            placeholder='Rechercher un champion...'
+            type='text'
+          />
+        </div>
+        {displayedChampions.length === 0 && (
+          <p className='Champions__no-data'>
+            Aucun champion ne correspond à votre recherche
+          </p>
+        )}
+        {displayedChampions.length > 0 && (
+          <ul>
+            {displayedChampions.map(champ => (
+              <li key={champ.key}>
+                <ChampCard {...champ} />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
