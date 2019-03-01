c4c061e5b7dff7a533f6ea3d204afe75a96f5eda
Faissal <fhattou@simplon.co>
2019-03-01 15:22:24 +0100

    Add api/ folder with requests to fetch data

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+export default {
+  GET_ALL_CHAMPIONS:
+    "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/fr_FR/champion.json",
+  CHAMPION_IMAGE: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash" // /{champion_0}.jpg
+};
