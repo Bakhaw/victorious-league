e7a22f693864c9eca93a4963bdd3e6c2b45eef98
Faissal <fhattou@simplon.co>
2019-03-01 16:54:30 +0100

    Add /champions & /champion-detail routes

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
+
+import NavBar from '../components/NavBar';
+import ChampionDetail from '../screens/ChampionDetail';
+import Champions from '../screens/Champions';
 import Home from '../screens/Home';
 
 export default function Router() {
   return (
     <HashRouter>
-      <Switch>
-        <Route path='/' component={Home} />
-      </Switch>
+      <>
+        <NavBar />
+        <Switch>
+          <Route
+            path='/champion-detail/:championName'
+            component={ChampionDetail}
+          />
+          <Route path='/champions' component={Champions} />
+          <Route path='/' component={Home} />
+        </Switch>
+      </>
     </HashRouter>
   );
 }
