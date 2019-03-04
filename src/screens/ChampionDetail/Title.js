32ebfffe8d23ec3faf969ecc3a8a4dedb3fd02de
bakhaw <fhattou@simplon.co>
2019-03-04 16:29:39 +0100

    Add title to ChampionDetail header

--- src/screens/ChampionDetail/Title.js
+++ src/screens/ChampionDetail/Title.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function Title({ name, tags }) {
+export default function Title({ name, title, tags }) {
   return (
     <div className='ChampionDetail__Title'>
       <h1>{name}</h1>
+      <span>{title}</span>
       <ul>{tags && tags.map((tag, index) => <li key={index}>{tag}</li>)}</ul>
     </div>
   );
