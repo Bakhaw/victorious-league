a0d8e19536edbf191b4f1bcb1a605110d5a80458
Hattou Faïssal <faissal.hattou@gmail.com>
2022-02-17 00:14:32 +0100

    Update ddragon API version (to 12.4.1) & fix css bug

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,15 @@
+// get last versions available here https://ddragon.leagueoflegends.com/api/versions.json
+
 export default {
   GET_ALL_CHAMPIONS: () =>
-    'http://ddragon.leagueoflegends.com/cdn/6.24.1/data/fr_FR/champion.json',
+    'http://ddragon.leagueoflegends.com/cdn/12.4.1/data/fr_FR/champion.json',
   GET_CHAMPION_DETAIL: championName => {
-    return `http://ddragon.leagueoflegends.com/cdn/6.24.1/data/fr_FR/champion/${championName}.json`;
+    return `http://ddragon.leagueoflegends.com/cdn/12.4.1/data/fr_FR/champion/${championName}.json`;
   },
   CHAMPION_IMAGE_BANNER: championName => {
     return `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championName}_0.jpg`;
   },
   CHAMPION_IMAGE_SQUARE: championName => {
-    return `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${championName}.png`;
+    return `http://ddragon.leagueoflegends.com/cdn/12.4.1/img/champion/${championName}.png`;
   }
 };
